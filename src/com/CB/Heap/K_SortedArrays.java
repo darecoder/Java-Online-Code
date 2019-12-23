80c5968a3291e0671fd5d5e25b376df097b36b68
ekta1999 <ektamishra1999@gmail.com>
2019-12-23 12:37:56 +0530

    Heaps added

--- src/com/CB/Heap/K_SortedArrays.java
+++ src/com/CB/Heap/K_SortedArrays.java
@@ -1,7 +1,21 @@
 package com.CB.Heap;
 
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+
 public class K_SortedArrays {
     public static void main(String[] args) {
+        int[][] nums = { {3, 7, 10, 15},
+                         {1, 6, 20, 32},
+                         {2, 3, 7, 26},
+                         {13, 27, 34, 44} };
 
+        ArrayList<Integer> list = mergeKSortedArray(nums);
+
+        System.out.println(list);
+    }
+
+    public static ArrayList<Integer> mergeKSortedArray(int[][] nums) {
     }
 }
