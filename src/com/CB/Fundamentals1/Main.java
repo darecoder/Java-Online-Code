c830fa59ed59bfe26477343350da5eee9cc957c8
ekta1999 <ektamishra1999@gmail.com>
2020-01-16 15:45:34 +0530

    add

--- src/com/CB/Fundamentals1/Main.java
+++ src/com/CB/Fundamentals1/Main.java
@@ -1,7 +1,22 @@
 package com.CB.Fundamentals1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String str = "xhiellohi";
+        System.out.println(removeHi(str, 0));
+    }
 
+    public static String removeHi(String str, int ind) {
+        if (ind + 1 >= str.length()){
+            return str;
+        }
+
+        if(ind+2 <= str.length() && str.substring(ind,ind+2).compareTo("hi") == 0){
+            return removeHi(str.substring(0,ind)+str.substring(ind+2), ind);
+        }else {
+            return removeHi(str, ind+1);
+        }
     }
 }
